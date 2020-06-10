a51e28f5dcb46e553a3115502889b8f89cf866d5
Adit <aditisvegeta@gmail.com>
2020-06-10 17:41:21 +0800

    adding some config files for little server

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Simple ts log');
+function test(n1, n2) {
+    return n1 + n2;
+}
+var x = 5;
+var y = 56.5;
+console.log(test(x, y));
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log('Simple ts log');
+function test(n1: number, n2: number)
+{
+return n1+n2;
+}
+
+var x = 5;
+var y =56.5;
+
+console.log(test(x,y));
