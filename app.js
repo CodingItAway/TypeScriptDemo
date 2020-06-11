1f2d73681f1dae0957b9b8b370a4fc5dce8191c2
Adit <aditisvegeta@gmail.com>
2020-06-11 08:31:18 +0800

    adding some more data types

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-function test(n1, n2) {
+function test(n1, n2, check, phrase) {
     return n1 + n2;
 }
 var x = 5;
-var y = 56.5;
-console.log(test(x, y));
+var y = 51.5;
+console.log(test(x, y, true, "Adit"));
+var someSampleObject = {
+    name: "Adit",
+    age: 31
+};
+console.log(someSampleObject.name);
--- app.ts
+++ app.ts
@@ -1,9 +1,19 @@
-function test(n1: number, n2: number)
+function test(n1: number, n2: number, check: boolean, phrase: String)
 {
 return n1+n2;
 }
 
 var x = 5;
-var y =56.5;
+var y =51.5;
 
-console.log(test(x,y));
+console.log(test(x,y,true,"Adit"));
+
+const someSampleObject : {
+    name : string,
+    age : number
+} = {
+    name : "Adit",
+    age : 31
+}
+
+console.log(someSampleObject.name);
